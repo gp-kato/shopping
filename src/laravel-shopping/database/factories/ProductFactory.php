d87ec94eede30f215107332648dba4ce9b5b1f34
gp-kato <kato@gp1000.jp>
2024-10-11 14:51:40 +0900

    imgフォルダから画像が出ない

--- src/laravel-shopping/database/factories/ProductFactory.php
+++ src/laravel-shopping/database/factories/ProductFactory.php
@@ -10,35 +10,17 @@ use Illuminate\Support\Str;
  */
 class ProductFactory extends Factory
 {
+    protected $model = \App\Models\Product::class;
+
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition()
-    {
-        $imagePaths = [
-            'img/87321qXnHCrwSlepvVQtLmkdezSPwcv3.jpg',
-            'img/AZeeBmlcuDiglCeNTpde6KgqoZebnbrs.jpg',
-            'img/cXQ4yhsNgdpaI4qBK7LqRFhSUTW0qJXA.jpg',
-            'img/GhPbey8IFqNzLtNTiQjnP0SeF4A6IJte.jpg',
-            'img/h8ydJHHmjr1zlKUHJZb0bbNohBwnuUt5.jpg',
-            'img/Loho4S04ZYKeIz5yS9qhLfQFTUfyfksx.jpg',
-            'img/LUPwAqNBaNUeL4GEwA17yQzdI25NtYYc.jpg',
-            'img/qG7Cezpi1PcXPJb1H46LwSYXwV4qBdoh.jpg',
-            'img/qSgZU0Df8TzNikYx74FEtiRUQjKYost5.jpg',
-            'img/snNhAFOzU00rSZbxBLjojsVH6nqlDAfj.jpg',
-            'img/VJJWjD2pgLarrmf1qSxCHxTSmumUklNM.jpg',
-            'img/x4CXhaIpOX5LPNYGeCqpJb2StITBKsxv.jpg',
-            'img/XGFZivxjQHDWlUGiA1DwrwUfWPTCSmme.jpg',
-            'img/YaRqDOLWhMEBNxfzLuCCvUJsRi3WysBb.jpg',
-            'img/yYe1EcEAHBlGH5K7NKWqy9BqLfdxCxsI.jpg',
-            'img/Z9fgLM6POkIwotNsXiaFMNkm2kh6gZB7.jpg',
-        ];
-                     
+    public function definition() {
         return [
             'name' => $this->faker->words(3, true), // ランダムなプロダクト名
-            'path' => $this->faker->randomElement($imagePaths), // 固定の画像パス
+            'path' => 'img/no_image.jpeg', // 画像パス
             'price'=> $this->faker->numberBetween(1000, 100000), // ランダムな価格
         ];
     }
--- src/laravel-shopping/database/seeders/ProductTableSeeder.php
+++ src/laravel-shopping/database/seeders/ProductTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use App\Models\Product;
 
 class ProductTableSeeder extends Seeder
 {
@@ -16,5 +18,15 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         \App\Models\Product::factory(16)->create();
+        
+        $images = File::files(public_path('img'));
+
+        foreach ($images as $image) {
+            Product::create([
+                'name' => pathinfo($image->getFilename(), PATHINFO_FILENAME),
+                'path' => 'img/' . $image->getFilename(),
+                'price' => random_int(1000, 100000), // Adding the price field
+            ]);
+        }
     }
 }
