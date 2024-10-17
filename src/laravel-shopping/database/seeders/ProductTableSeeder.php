1d88913744dc608f82cb4bb03bf4f368ee55ddb0
gp-kato <kato@gp1000.jp>
2024-10-17 13:58:46 +0900

    C:/Program Files (x86)/Git/productから表示

--- src/laravel-shopping/database/seeders/ProductTableSeeder.php
+++ src/laravel-shopping/database/seeders/ProductTableSeeder.php
@@ -16,13 +16,20 @@ class ProductTableSeeder extends Seeder
      * @return void
      */
     public function run() {
-        \App\Models\Product::factory(16)->create();
+        $products = \App\Models\Product::factory(16)->create();
 
-        $images = File::files(public_path('img'));
+        $images = File::files(public_path('img/products'));
 
         $paths = [];
         foreach ($images as $image) {
             $paths[] = 'img/products/' . $image->getFilename();
+        }
+
+        foreach ($products as $index => $product) {
+            // パスがある場合は、製品に画像を関連付ける
+            if (isset($paths[$index])) {
+                $product->update(['path' => $paths[$index]]);
+            }
         }
     }
 }
